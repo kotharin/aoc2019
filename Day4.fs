95627773cf718a4acb25c75798c4f1e6869d1e34
Neerav Kothari <neerav.kothari@codeeight.co>
2020-01-17 15:00:45 -0500

    Day4 part2

--- Day4.fs
+++ Day4.fs
@@ -28,22 +28,30 @@ module Part2 =
     let isSixDigit i =
         (i > 99999) && (i < 1000000)
 
-    let hasAdjacentSameDigitsAndNonDecreasing i =
-        let s = i.ToString().ToCharArray()
 
-        let adjacent, decreasing, _ =
-            s
-            |> Array.fold (fun adp n -> 
-                let a,d,p = adp
-                ((a || (p = n)) && (not a)), (d && (p<=n)) ,n
-                ) (false, true,'0')
-        adjacent && decreasing
+    let rec adjacentEqualRec ln =
+        match ln with
+        | a::b::c::d::e::[f] ->
+            (((a=b) && (b <> c) || ((a<>b) && (b = c) && (c <> d)) || ((b <> c) && (c = d) && (d <> e))|| ((c <> d) && (d = e) && (e <> f)) || ((d <> e) && (e = f)) ))
+        | _ ->
+            false
+
+    let rec nonDecreasing ln =
+        match ln with
+        | a::[b] -> 
+            a <= b
+        | a::b::c -> 
+            (a <= b) && (nonDecreasing (b::c))
+        | _ -> 
+            false
 
     let solution = 
         let range = [307237..769058]
-        //let range = [112233]
+        //let range = [677788]
 
         range
         |> Seq.filter isSixDigit
-        |> Seq.filter hasAdjacentSameDigitsAndNonDecreasing
+        |> Seq.map (fun n -> n.ToString().ToCharArray() |> List.ofArray)
+        |> Seq.filter nonDecreasing
+        |> Seq.filter adjacentEqualRec
         |> Seq.length
--- Program.fs
+++ Program.fs
@@ -4,6 +4,6 @@ open System
 open Day4
 [<EntryPoint>]
 let main argv =
-    
+
     printfn "%A" Part2.solution
     0 // return an integer exit code
